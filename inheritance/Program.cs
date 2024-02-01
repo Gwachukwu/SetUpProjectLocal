d2da6e81e564ac9a1132a64dc6a03a2b7780483e
gwachukwu <gwachukwuelebeke@gmail.com>
2024-01-31 20:46:03 -0500

    today's job

---
+++ inheritance/Program.cs
@@ -0,0 +1,23 @@
+﻿// See https://aka.ms/new-console-template for more information
+// Console.WriteLine("Hello, World!");
+
+Age me = new Age();
+Name mainName = new Name();
+
+mainName.SayName();
+me.SayName();
+public class Name
+{
+    public virtual void SayName()
+    {
+        Console.WriteLine("My name is Gwachukwu");
+    }
+}
+public class Age : Name
+{
+    public override void SayName()
+    {
+        // base.SayName();
+        Console.WriteLine("My name is Dade");
+    }
+}
